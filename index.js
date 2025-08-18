1c3831384c05a7ab5aee4157db5e3522bb255bb7
naxor <naxordeve@gmail.com>
2025-08-18 16:10:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const face = require("./core");
-const app = express();
-
-app.use("/fb", face);
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
