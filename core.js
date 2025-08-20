8034286fe949bd45388b8d7d9df38484d3acbf69
naxor <naxordeve@gmail.com>
2025-08-20 13:03:31 +0200

    naxor

--- core.js
+++ core.js
@@ -53,6 +53,35 @@ app.get('/search/anime', async (req, res) => {
 });
 
 
+// ======================= TWITTER DOWNLOADER =======================
+app.get('/download/twitter', async (req, res) => {
+  const { url } = req.query;
+  if (!url) return res.status(400).json({ owner: "naxordeve", error: 'Missing url' });
+  try {const config = { url };
+    const { data } = await axios.post(
+      'https://www.expertsphp.com/instagram-reels-downloader.php',
+      qs.stringify(config),
+      {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'cookie': '_gid=GA1.2.1209552833.1682995186; _gat_gtag_UA_120752274_1=1; __gads=ID=e2d27851a97b70ac-222d68fe87e000b0:T=1682995185:RT=1682995185:S=ALNI_MYaXoBa8KWleDZ97JpSaXGyI7nu3g; __gpi=UID=00000be71a67625d:T=1682995185:RT=1682995185:S=ALNI_MYyedH9xuRqL2hx4rg7YyeBDzK36w; _ga_D1XX1R246W=GS1.1.1682995185.1.1.1682995205.0.0.0; _ga=GA1.1.363250370.1682995185',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+        },
+      }
+    );
+
+    const $ = cheerio.load(data);
+    const videoUrl = $('div.col-md-4.col-md-offset-4 > table > tbody > tr > td > video').attr('src');
+    if (!videoUrl) {
+      return res.status(404).json({ owner: "naxordeve", error: 'No video found. Maybe the service changed?' });
+    }
+
+    res.json({ owner: "naxordeve", success: true, video: videoUrl });
+
+  } catch (err) {
+    res.status(500).json({ owner: "naxordeve", error: err.message });
+  }
+});
 
 app.get("/spotify/search", async (req, res) => {
   const query = req.query.q
